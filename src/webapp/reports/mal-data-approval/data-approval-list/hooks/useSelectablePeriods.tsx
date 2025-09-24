06c1593fba7d65619963e4f66e9750170026a89f
Eduardo Peredo Rivero <jrperedo@gmail.com>
2025-09-24 17:12:16 -0500

    update recent periods from 2020 to current year minus 1

--- src/webapp/reports/mal-data-approval/data-approval-list/hooks/useSelectablePeriods.tsx
+++ src/webapp/reports/mal-data-approval/data-approval-list/hooks/useSelectablePeriods.tsx
@@ -7,7 +7,7 @@ export function useSelectablePeriods(useOldPeriods: boolean): string[] {
 
         return useOldPeriods
             ? _.range(START_YEAR, currentYear - 5).map(n => n.toString())
-            : _.range(currentYear - 5, currentYear + 6).map(n => n.toString());
+            : _.range(currentYear - 5, currentYear).map(n => n.toString());
     }, [useOldPeriods]);
 
     return selectablePeriods;
